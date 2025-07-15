22a0cc1c7e564f9018e402dbf2de17ae9d9ce785
MouhamedDridii <mouhamed.eldridi@gmail.com>
2025-07-15 15:59:42 +0100

    add boostrap form config in config/packages/ twig.yaml and add createForm in the edit function in REcipeController

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Recipe;
+use App\Form\RecipeType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -19,9 +20,10 @@ final class RecipeController extends AbstractController
     #[Route('/recipe/{id}/edit', name: 'recipe.edit')]
     public function edit(Recipe $recipe ): Response// le framework symfony connait que dans le route tu cherche sur l'id donc elle select l'entite avec l'id passer au parametre(cette methode n'est valable qu'avec le champ id)
     {
-        dd($recipe);
-        return $this->render('recipe/index.html.twig', [
-            'controller_name' => 'RecipeController',
+        $form=$this->createForm(RecipeType::class,$recipe);//j'ai cree un formulaire avec les donner de l'entite recipe passer en parametre dans la fonction
+        return $this->render('recipe/edit.html.twig', [
+            'recipe' => $recipe,
+            'form'   => $form,
         ]);
     }
 }
