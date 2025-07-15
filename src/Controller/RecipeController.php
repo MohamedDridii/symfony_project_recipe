d2dff57eb49e37af98b1380544a19cc813dcd1f9
MouhamedDridii <mouhamed.eldridi@gmail.com>
2025-07-15 23:31:12 +0100

    add recipe/create.html.twig to show the form,create a method named create in RecipeControllerps:everything works fine

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Recipe;
 use App\Form\RecipeType;
+use DateTime;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -27,7 +29,8 @@ final class RecipeController extends AbstractController
         $form->handleRequest($request);//ca modifie automatiquement les valeurs ajouter au formulaie dans l'entite puisque chaque champ du formulaire et lie a une entite lors de la creation du formulaire on peut voir les detail dans RecipeType dans la methode configureOptions
         
         if ($form->isSubmitted() && $form->isValid()){
-            $em->flush();//maintenant on va modifier lesdonner soummit par le formulaire dans la base de donner 
+            $em->flush();//maintenant on va modifier lesdonner soummit par le formulfaire dans la base de donner 
+            $this->addFlash('message','la recette a bien été modifiée ');
             return $this->redirectToRoute('app_recipe');//redirection vers la page avec le nom de route app_route
         }
         
@@ -36,4 +39,27 @@ final class RecipeController extends AbstractController
             'form'   => $form,
         ]);
     }
+    #[Route('/recipe/create',name:'recipes.create')]
+    public function create( Request $request,EntityManagerInterface $em,):Response{// on n'avait pas ajouter recipe au parametre de cette fonction car on vas cree et persister la recette or dans la fct precedante on a deja la recette et on veulent la modifier(l'utilisation de l'entite au parametre dans la fonction precedante etait une autre methode explique deja )
+        $recipe= new Recipe();
+        $form = $this->createForm(RecipeType::class,$recipe);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $recipe->setCreatedat(new DateTimeImmutable());
+            $recipe->setUpdatedAt(new DateTimeImmutable());
+            $em->persist($recipe);
+            $em->flush();
+            $this->addFlash('success','la recette a bien été crée');
+            return $this->redirectToRoute('app_recipe');
+        }
+        return $this->render('recipe/create.html.twig',[//ici on a passer seulement le formulaire au view car on n'affichera rien 
+            'form'=>$form
+        ]);
+    }
+
+
+
+
+
+
 }
